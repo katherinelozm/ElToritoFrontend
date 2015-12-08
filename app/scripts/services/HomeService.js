5c8da74c6ea03c4fc335338215e8f74ff1beafa0
Katherine Lozano <katherinelozm@gmail.com>
2015-12-08 09:16:58 -0600

    Change base url

--- app/scripts/services/HomeService.js
+++ app/scripts/services/HomeService.js
@@ -1,7 +1,7 @@
 angular.module('ElTorito.Services').factory('HomeService', ['$http',
 	function($http){
 		$http.defaults.withCredentials = true;
-		var baseUrl = 'http://localhost:8000/';
+		var baseUrl = 'https://eltoritofrontend.herokuapp.com/';
 		return {
 				GetUsers: function(){
 					return $http.get(baseUrl + "v1/users");
