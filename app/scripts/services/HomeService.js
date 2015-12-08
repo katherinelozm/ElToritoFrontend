a4e495413a6f66a4b9cc9b676f7946ea3bb93184
Katherine Lozano <katherinelozm@gmail.com>
2015-12-08 08:48:33 -0600

    Frontend only

---
+++ app/scripts/services/HomeService.js
@@ -0,0 +1,19 @@
+angular.module('ElTorito.Services').factory('HomeService', ['$http',
+	function($http){
+		$http.defaults.withCredentials = true;
+		var baseUrl = 'http://localhost:8000/';
+		return {
+				GetUsers: function(){
+					return $http.get(baseUrl + "v1/users");
+				},
+				Logout: function(){
+					return $http.get(baseUrl + "v1/logout");
+				},
+				Login: function(payload){
+					return $http.post(baseUrl + "v1/login", payload);
+				},
+		        Register: function(payload){
+		          return $http.post(baseUrl + "v1/register", payload);
+		        }
+	    };
+}]);
