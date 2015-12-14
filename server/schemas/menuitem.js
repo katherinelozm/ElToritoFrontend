161e5696c0f4c1f05e54944698ff61fb7624e22a
Gisselle Lagos <gisselle.lagos@unitec.edu>
2015-12-13 21:49:16 -0600

    View for register new users (all scopes)

--- server/schemas/menuitem.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var MenuItemSchema = new mongoose.Schema({
-  name : String,
-  price : String,
-  category : String,
-});
-
-module.exports = mongoose.model('MenuItem', MenuItemSchema);
