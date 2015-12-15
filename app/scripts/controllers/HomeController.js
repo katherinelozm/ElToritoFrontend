e3b3b90444d10c97a596fcf0f96d375af68998fb
Katherine Lozano <katherinelozm@gmail.com>
2015-12-15 00:35:08 -0600

    Views waiter

--- app/scripts/controllers/HomeController.js
+++ app/scripts/controllers/HomeController.js
@@ -3,6 +3,14 @@ angular.module('ElTorito.Controllers')
       $scope.user = {};
       $scope.$sessionStorage = $sessionStorage;
 
+      $scope.isAdmin = function(){
+        return $sessionStorage.currentUser && $sessionStorage.currentUser.scope.indexOf('admin') > -1;
+      }
+
+      $scope.isClient = function(){
+        return $sessionStorage.currentUser && $sessionStorage.currentUser.scope.indexOf('client') > -1;
+      }
+
       $scope.logout = function(){
         HomeService.Logout().then(function(response){
           alert('logged out correctly');
@@ -16,7 +24,7 @@ angular.module('ElTorito.Controllers')
         HomeService.Login(user).then(function(response){
           $sessionStorage.currentUser = response.data;
           $scope.user = {};
-          $state.go('client')
+          $state.go('waiter')
         }).catch(function(err){
           alert(err.data.error + " " + err.data.message);
         });
@@ -50,11 +58,35 @@ angular.module('ElTorito.Controllers')
         $state.go('client')
       }
 
-      $scope.isAdmin = function(){
-        return $sessionStorage.currentUser && $sessionStorage.currentUser.scope.indexOf('admin') > -1;
+      $scope.Waiter = function(){
+        $state.go('waiter')
       }
 
-      $scope.isClient = function(){
-        return $sessionStorage.currentUser && $sessionStorage.currentUser.scope.indexOf('client') > -1;
+      $scope.Order = function(){
+        $state.go('order')
+      }
+
+      $scope.Appetizers = function(){
+        $state.go('appetizers')
+      }
+
+      $scope.ChickenAndFish = function(){
+        $state.go('chickenandfish')
+      }
+
+      $scope.Shrimps = function(){
+        $state.go('shrimps')
+      }
+
+      $scope.FromTheGrill = function(){
+        $state.go('fromthegrill')
+      }
+
+      $scope.Internationals = function(){
+        $state.go('internationals')
+      }
+
+      $scope.Drinks = function(){
+        $state.go('drinks')
       }
   }]);
